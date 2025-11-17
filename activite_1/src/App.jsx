e891c6a9baa35607919e65cc548e2c390aa79e9f
Omar Jabour <jabour.omar@hotmail.com>
2025-11-17 12:11:28 +0100

    activite 4

--- activite_1/src/App.jsx
+++ activite_1/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
+import NotFound from "./pages/NotFound";
 import Layout from "./components/Layout";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/categorie/:category" element={<Category />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
