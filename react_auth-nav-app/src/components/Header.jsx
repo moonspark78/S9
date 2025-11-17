721e2d9e6977a9c3d268aebc5dd0972503603b24
Omar Jabour <jabour.omar@hotmail.com>
2025-11-17 14:36:14 +0100

    activite 5 fix add header

--- react_auth-nav-app/src/components/Header.jsx
+++ react_auth-nav-app/src/components/Header.jsx
@@ -3,26 +3,28 @@ import { Link } from "react-router";
 
 function Header() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/" className="cursor-pointer">
-              Accueil
-            </Nav.Link>
+    <header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/" className="cursor-pointer">
+                Accueil
+              </Nav.Link>
 
-            <Nav.Link as={Link} to="/categorie/vetements" className="cursor-pointer">
-              Vêtements
-            </Nav.Link>
+              <Nav.Link as={Link} to="/categorie/vetements" className="cursor-pointer">
+                Vêtements
+              </Nav.Link>
 
-            <Nav.Link as={Link} to="/categorie/chaussures" className="cursor-pointer">
-              Chaussures
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+              <Nav.Link as={Link} to="/categorie/chaussures" className="cursor-pointer">
+                Chaussures
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
 
