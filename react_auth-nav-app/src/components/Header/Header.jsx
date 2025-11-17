2e7af0416d3638ceb2d6fbb0980412ca50c57816
Omar Jabour <jabour.omar@hotmail.com>
2025-11-17 16:29:30 +0100

    activite 7

--- react_auth-nav-app/src/components/Header/Header.jsx
+++ react_auth-nav-app/src/components/Header/Header.jsx
@@ -1,12 +1,22 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { NavLink } from "react-router";
+import { NavLink, useMatch } from "react-router";
 import "./Header.css";
 
 function Header() {
+  const match = useMatch("/categorie/:category");
+  const categoryName = match ? match.params.category : null;
+
+
   return (
     <header>
       <Navbar bg="light" expand="lg">
         <Container>
+          {categoryName && (
+            <span className="me-3 fw-bold">
+              Catégorie : {categoryName.charAt(0).toUpperCase() + categoryName.slice(1)}
+            </span>
+          )}
+          
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
