9000b77f5ad59340385253911a95660b4ecf09ab
Omar Jabour <jabour.omar@hotmail.com>
2025-11-17 17:12:12 +0100

    activite 8

--- react_auth-nav-app/src/pages/Category.jsx
+++ react_auth-nav-app/src/pages/Category.jsx
@@ -1,12 +1,21 @@
-import { useParams } from "react-router";
+import { useParams,useNavigate } from "react-router";
 import { Container } from "react-bootstrap";
 
 function Category() {
   const { category } = useParams();
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate("/");
+  };
+
 
   return (
     <Container className="ms-10">
       <h1>Catégorie : {category}</h1>
+      <button onClick={goHome} className="btn btn-primary mt-3">
+        Retour à l'accueil
+      </button>
     </Container>
   );
 }
