8980881cb615d19917bcbd3bbf96e751917ceb72
Omar Jabour <jabour.omar@hotmail.com>
2025-11-17 15:49:19 +0100

    activite 6 fix foder

--- react_auth-nav-app/src/components/Layout.jsx
+++ react_auth-nav-app/src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import Header from "./Header";
+import Header from "./Header/Header";
 import Footer from "./Footer";
 import { Outlet } from "react-router";
 import { Container } from "react-bootstrap";
