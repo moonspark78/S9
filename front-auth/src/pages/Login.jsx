5ec5c2e82aa79ac0850195a33aa573dcbe746074
Omar Jabour <jabour.omar@hotmail.com>
2025-11-18 16:31:22 +0100

    :art: feat(login): ajout de la requête de connexion + gestion des erreurs 401 et 404

--- front-auth/src/pages/Login.jsx
+++ front-auth/src/pages/Login.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
-import { Form, Button, Container, Card, Row, Col } from "react-bootstrap";
+import { Form, Button, Container, Card, Row, Col, Alert  } from "react-bootstrap";
+import { useNavigate } from "react-router";
 
 const LoginPage = () => {
+   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
 
   const handleChange = (e) => {
     setFormData({
@@ -14,13 +19,43 @@ const LoginPage = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    setError(null);
+    setLoading(true);
+
     // Handle login logic here
+    try {
+      const response = await fetch("https://offers-api.digistos.com/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
     // Don't forget to handle errors, both for yourself (dev) and for the client (via a Bootstrap Alert):
+     if (!response.ok) {
     //   - Show an error if credentials are invalid
+     if (response.status === 401) {
+          throw new Error("Email ou mot de passe incorrect.");
     //   - Show a generic error for all other cases
+     } else {
+          // Sinon erreur générique
+          throw new Error(`Erreur ${response.status} - ${data.message || "Une erreur est survenue."}`);
+        }
+      }
     // On success, redirect to the Pro Offers page
+     navigate("/offres/professionnelles");
+     } catch (err) {
+      console.error(err);
+      setError("Une erreur est survenue. Veuillez vérifier vos identifiants et réessayer.");
+    } finally {
+      setLoading(false);
+    }
+
     console.log("Login submitted:", formData);
   };
 
@@ -30,6 +65,7 @@ const LoginPage = () => {
         <Col xs={12} sm={8} md={6} lg={4}>
           <Card className="p-4 shadow-lg">
             <h1 className="text-center mb-4">Se connecter</h1>
+             {error && <Alert variant="danger" className="text-center">{error}</Alert>}
             <Form onSubmit={handleSubmit}>
               <Form.Group className="mb-3" controlId="loginEmail">
                 <Form.Label>Email</Form.Label>
